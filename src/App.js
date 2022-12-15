f937321db5d7445c22c067010a3c0e899dc22dee
Satyaprakash Das <satyadasdipu5@gmail.com>
2022-12-15 19:02:42 +0530

    Footer Updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Pages/HomePage/Home';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Pages/Footer/Footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
     </>
   );
